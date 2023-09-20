1dd9c1ad3273a20f158646db414bc730e5157771
MrStnslsv <stanislav.o@ideainyou.com>
2023-09-20 18:38:52 +0300

    commit

--- app/db_connection.php
+++ app/db_connection.php
@@ -1,5 +1,5 @@
 <?php
- $connection = mysqli_connect('localhost', 'root', 'Sllt3rd_cs', 'home_db');
+ $connection = mysqli_connect('localhost', 'root', 'root', 'home_db');
     if (!$connection) {
         die('Error db connection');
     }
